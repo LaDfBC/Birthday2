9b8e957de70dd1dd3c64a25b40c8d278b92c7d56
George <george.mausshardt@mst.edu>
2017-10-18 21:47:41 -0500

    Added Enum - must continue on Linux machine for Pip Support

---
+++ data/Dailies.py
@@ -0,0 +1,7 @@
+__author__ = 'George'
+
+from aenum import MultiValueEnum
+
+class Message(MultiValueEnum):
+    JANUARY_1 = 1, 1, "Happy New Year, babe!  Hope you had a good night last night!  Thinking of you through the New Year!  Since we're both off work today, let's go have some fun!",
+    JANUARY_2 = 1, 2, "",

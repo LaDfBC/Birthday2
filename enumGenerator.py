ba96b442edfa1291902c6d634bba62dc8b11ea5f
George <george.mausshardt@mst.edu>
2017-10-27 17:23:03 -0500

    First successful RTM Call

--- enumGenerator.py
+++ enumGenerator.py
@@ -1,14 +1,22 @@
+import json
+
 __author__ = 'George'
 
 def makeFile():
     months_names = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
     months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    file = open("C:\\Users\\George\\PycharmProjects\\months.txt", 'w+')
+    dict = {}
+
+    file = open("/home/squeaky/months.txt", 'w+')
     for month_number in range (0, 12):
         month_name = months_names[month_number]
+
+        dict[month_number + 1] = {}
         for day in range(1, months[month_number] + 1):
-            file.write(str(month_name) + "_" + str(day) + " = " + str(month_number + 1) + ", " + str(day) + "," + '"",' + "\n")
+            dict[month_number + 1][day] = ""
+
+    file.write(json.dumps(dict))
 
     file.close()
 
--- util/TaskAdder.py
+++ util/TaskAdder.py
@@ -1,6 +1,51 @@
 __author__ = 'George'
 import requests
+import hashlib
 
-url = 'https://api.rememberthemilk.com/services/rest/'
-def add_task(task_name, task_notes):
-    response = requests.get(url, auth)
+BASE_URL = 'https://api.rememberthemilk.com/services/rest/'
+
+
+METHOD_PARAM = "method"
+APIKEY_PARAM = "api_key"
+APISIGNATURE_PARAM = "api_sig"
+
+def get_list():
+    args = {}
+    method = "rtm.lists.getList"
+    args[APIKEY_PARAM] = API_KEY
+    args[METHOD_PARAM] = method
+    args["format"] = "json"
+    print(requests.get(BASE_URL, args))
+
+def test():
+    args = {}
+    method = "rtm.test.echo"
+    args[APIKEY_PARAM] = API_KEY
+    args[METHOD_PARAM] = method
+    args["format"] = "json"
+    api_sig = APIKEY_PARAM + API_KEY + METHOD_PARAM
+    print(requests.get(BASE_URL +
+                            "?" +
+                            "method=" + method + "&" +
+                            "api_key" + api_key + "&" +
+                            "format=json").text)
+
+def get_frob():
+    args = {}
+    method = "rtm.auth.getFrob"
+    args[APIKEY_PARAM] = API_KEY
+    args[METHOD_PARAM] = method
+    api_sig_solution = API_SECRET + APIKEY_PARAM + API_KEY + METHOD_PARAM + method
+    signature = hashlib.md5(api_sig_solution.encode("utf-8"))
+    url = BASE_URL + "?" + \
+                       APIKEY_PARAM + "=" + API_KEY + "&" + \
+                       APISIGNATURE_PARAM + "=" + signature.hexdigest() + "&" + \
+                       METHOD_PARAM + "=" + method
+    print(url)
+    print(requests.get(url).text)
+
+get_frob()
+# get_list()
+
+
+# add_task("test_name", "test_notes")
